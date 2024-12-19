ef8b4bfc69a023004aabf3c4e9389643ac4f8590
MarkGyure <mgyure@mail.bradley.edu>
2024-12-18 20:56:37 -0600

    Initial commit

---
+++ src/main/java/net/myce/warcraft/WarCraftClient.java
@@ -0,0 +1,10 @@
+package net.myce.warcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WarCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/myce/warcraft/WarCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.myce.warcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WarCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
