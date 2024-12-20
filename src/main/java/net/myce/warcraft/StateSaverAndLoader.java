0c1b0db4f4f206ef884233cfaf5fba7942d5ad88
brycehar <bryce.harbison@icloud.com>
2024-12-20 14:26:57 -0600

    Perfection

--- src/main/java/net/myce/warcraft/StateSaverAndLoader.java
+++ src/main/java/net/myce/warcraft/StateSaverAndLoader.java
@@ -1,6 +1,7 @@
 package net.myce.warcraft;
 
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.PersistentState;
