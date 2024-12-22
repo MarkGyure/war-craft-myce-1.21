4e8427aa965c372065f39ee59be0e083569bfdc4
brycehar <bryce.harbison@icloud.com>
2024-12-21 19:17:09 -0600

    yes

--- src/main/java/net/myce/warcraft/WarCraft.java
+++ src/main/java/net/myce/warcraft/WarCraft.java
@@ -4,6 +4,8 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.myce.warcraft.block.ModBlocks;
 import net.myce.warcraft.events.ClaimStoneChunkHandler;
@@ -12,6 +14,8 @@ import net.myce.warcraft.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+import java.util.List;
 
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
@@ -33,31 +37,82 @@ public class WarCraft implements ModInitializer {
 
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
 				dispatcher.register(literal("createfaction") //phrase/word used to call command
-					.then(argument("name", StringArgumentType.string()) //creates the argument that asks for the name of the faction
-							.executes(context ->
-							{
-								final String name = StringArgumentType.getString(context, "name"); //creates a string that gets the argument
+						.then(argument("name", StringArgumentType.string()) //creates the argument that asks for the name of the faction
+								.executes(context ->
+								{
+									StateSaverAndLoader serverState = StateSaverAndLoader.getServerState(context.getSource().getServer());
+									PlayerData playerData = StateSaverAndLoader.getPlayerState(context.getSource().getPlayer());
 
-								StateSaverAndLoader serverState = StateSaverAndLoader.getServerState(context.getSource().getServer());
-								PlayerData playerData = StateSaverAndLoader.getPlayerState(context.getSource().getPlayer());
+									final String name = StringArgumentType.getString(context, "name"); //creates a string that gets the argument
+									final boolean available = Factions.checkIfFactionNameAvailable(name, serverState);
+									final boolean notInAFaction = Factions.checkIfAlreadyInAFaction(playerData);
 
-								playerData.factionName = name;
-								serverState.factionList.add(name);
+									if(available && notInAFaction)
+									{
+										serverState.factionList.add(name);
+										playerData.factionName = name;
+										playerData.factionLeader = true;
 
-								context.getSource().sendFeedback(() -> Text.literal("Your faction " + name + " is created."), false);
+										context.getSource().sendFeedback(() -> Text.literal("Your faction " + name + " is created."), false);
+									}
+									else if(!available)
+										context.getSource().sendFeedback(() -> Text.literal("Faction name is not available."), false);
+									else
+										context.getSource().sendFeedback(() -> Text.literal("You must leave the faction you're currently in first."), false);
 
-								return 1;
-							}))));
+									return 1;
+								}))));
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
+				dispatcher.register(literal("joinfaction") //phrase/word used to call command
+						.then(argument("name", StringArgumentType.string()) //creates the argument that asks for the name of the faction
+								.executes(context ->
+								{
+									StateSaverAndLoader serverState = StateSaverAndLoader.getServerState(context.getSource().getServer());
+									PlayerData playerData = StateSaverAndLoader.getPlayerState(context.getSource().getPlayer());
+
+									final String name = StringArgumentType.getString(context, "name");
+									final boolean notInAFaction = Factions.checkIfAlreadyInAFaction(playerData);
+									final boolean factionExists = Factions.checkIfFactionExists(name, serverState);
+									ServerPlayerEntity player = null;
+
+									if(notInAFaction && factionExists)
+									{
+										context.getSource().sendFeedback(() -> Text.literal("Step 1"), false);
+										List<ServerPlayerEntity> listOfPlayers = context.getSource().getWorld().getPlayers();
+
+										for(ServerPlayerEntity listOfPlayer : listOfPlayers)
+										{
+											if (listOfPlayer.getUuid() == Factions.getFactionLeader(name, serverState))
+											{
+												player = listOfPlayer;
+											}
+										}
+
+										ServerPlayNetworking.send(context.getSource().getPlayer(), new Payload(context.getSource().getPlayer().getName().toString()));
+									}
+									else if(!notInAFaction)
+									{
+										context.getSource().sendFeedback(() -> Text.literal("You must leave the faction you're currently in first."), false);
+									}
+									else
+									{
+										context.getSource().sendFeedback(() -> Text.literal("No faction exists with this name."), false);
+									}
+
+									return 1;
+								}))));
 
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
 				dispatcher.register(literal("factioninfo")
+						.then(argument("name", StringArgumentType.string()))
 						.executes(context ->
 						{
 							PlayerData playerData = StateSaverAndLoader.getPlayerState(context.getSource().getPlayer());
 
 							context.getSource().sendFeedback(() -> Text.literal(playerData.factionName), false);
 
-							return 2;
+							return 1;
 						})));
 
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
@@ -72,7 +127,7 @@ public class WarCraft implements ModInitializer {
 								context.getSource().sendFeedback(() -> Text.literal(serverState.factionList.get(index)), false);
 							}
 
-							return 3;
+							return 1;
 						})));
 	}
 
